576490341f3f0e7b341f53a891be164eb6b1c082
alxgllt <alexandreguillot89@gmail.com>
2017-02-08 15:19:15 +0100

    back end done, starting front

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  RESTAURANT = ["chinese","italian", "japanese","french","belgian"]
+  validates :category, presence:true, inclusion: { in: RESTAURANT, allow_nil: false }
+  validates :name, presence: true
+  validates :address, presence: true
+
 end
